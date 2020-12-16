ad954657ac15ae1d37ee084361cd96e31d70b187
Marcelo <marcelocaj@gmail.com>
2020-12-16 23:19:57 +0100

    Update entrypoint.sh
    url of common repo set as variable

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,6 @@ cd /code/workspace
 
 
 git config --global credential.helper "/bin/bash /credentials-helper.sh"
-git clone http://192.168.116.100:30000/cstc/pstb/common.git
+git clone $COMMON_REPO_URL
+#http://192.168.116.100:30000/cstc/pstb/common.git
 
